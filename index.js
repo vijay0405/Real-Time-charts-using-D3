c57e22215b5d66a390b3a4d6cd1cfd9ca6b8523c
vijay0405 <uvijaysimha@gmail.com>
2020-06-19 23:57:53 +0530

    added socket.io with 5 sec interval to emit

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const data = require('./data');
+const io = require('socket.io')(http);
+
 
 const port = 3022;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 app.get('/api/data', (req, res) => {
-  res.send(data.data);
+    res.send(data.data);
+});
+
+setInterval(function () {
+    data.updateData();
+    io.sockets.emit('data', data.data[0]);
+}, 5000);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
 });
 
 http.listen(port, () => {
-  console.log(`Listening on *:${port}`);
+    console.log(`Listening on *:${port}`);
 });
