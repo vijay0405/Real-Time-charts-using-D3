8ce1e7a8cd322e605d51781c579c05101cbe9f39
vijay0405 <uvijaysimha@gmail.com>
2020-06-20 23:28:12 +0530

    getUpdates function added in status.service

--- src/app/status.service.ts
+++ src/app/status.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject, from } from  'rxjs';
+import * as socketio from 'socket.io-client';
 
 
 export  class Data {
@@ -19,5 +21,17 @@ export class StatusService {
 
   getInitialStatus() {
     return this.httpClient.get<Data[]>(`${this.baseUrl}/api/data`);
+  }
+
+  getUpdates() {
+    let socket = socketio(this.baseUrl);
+    let dataSub = new Subject<Data>();
+    let dataSubObservable = from(dataSub);
+
+    socket.on('data', (status: Data) => {
+      dataSub.next(status);
+    });
+
+    return dataSubObservable;
   }
 }
